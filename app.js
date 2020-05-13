f490c9db61bec4846c9cbc626f79fce7e83380d4
roubalsehgal <roubalsehgal2000@gmail.com>
2020-05-13 16:07:10 +0530

    Added First Version - Meditation App

---
+++ app.js
@@ -0,0 +1,79 @@
+const app = () => {
+    const song = document.querySelector('.song');
+    const play = document.querySelector('.play');
+    const outline = document.querySelector('.moving-outline circle');
+    const video = document.querySelector('.vid-container video');
+
+    // Sounds
+    const sounds = document.querySelectorAll('.sound-picker button');
+    // Time Display
+    const timeDisplay = document.querySelector('.time-display');
+    const timeSelect = document.querySelectorAll('.time-select button');
+    // Get the length of the outline
+    const outlineLength = outline.getTotalLength();
+    console.log(outlineLength);
+    // Duration
+    let fakeDuration = 600;
+
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = outlineLength;
+
+    // Pick Different Sounds
+    sounds.forEach(sound => {
+        sound.addEventListener('click', function () {
+            song.src = this.getAttribute('data-sound');
+            video.src = this.getAttribute('data-video');
+            checkPlaying(song);
+        })
+    })
+
+    // Play Sounds
+    play.addEventListener('click', () => {
+        checkPlaying(song);
+    });
+
+    // Select Sound
+    timeSelect.forEach(option => {
+        option.addEventListener('click', function () {
+            fakeDuration = this.getAttribute("data-time");
+            timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`;
+        });
+    });
+
+    // To Stop/Play the sounds
+    const checkPlaying = () => {
+        if (song.paused) {
+            song.play();
+            video.play();
+            play.src = "./svg/pause.svg";
+        } else {
+            song.pause();
+            video.pause();
+            play.src = "./svg/play.svg";
+        }
+    };
+
+    // Time
+    song.ontimeupdate = () => {
+        let currentTime = song.currentTime;
+        let elapsed = fakeDuration - currentTime;
+        let seconds = Math.floor(elapsed % 60);
+        let minutes = Math.floor(elapsed / 60);
+
+        // Animate the Circle 
+        let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+        outline.style.strokeDashoffset = progress;
+
+        // Text Animation
+        timeDisplay.textContent = `${minutes}:${seconds}`;
+
+        if (currentTime >= fakeDuration) {
+            song.pause();
+            song.currentTime = 0;
+            play.src = './svg/play.svg';
+            video.pause();
+        }
+    };
+};
+
+app();
